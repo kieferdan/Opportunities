edd6b09f7543577c022435ba912b4f310c04a912
Daniel Kiefer <danielfmpinho@gmail.com>
2023-07-28 23:26:55 -0300

    adjusts

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/kieferdan/Opportunities/router"
+import (
+	"github.com/kieferdan/Opportunities/config"
+	"github.com/kieferdan/Opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
 
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+
+	}
 	// Initialize Router
 	router.Initialize()
 }
